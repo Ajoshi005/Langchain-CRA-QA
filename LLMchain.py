47a96d5f26d9e4c74d579b5fd2e83a8a5b1a9443
Ajoshi005 <116526886+Ajoshi005@users.noreply.github.com>
2023-07-23 02:29:38 -0400

    requirements file and LLMchain.py files

--- LLMchain.py
+++ LLMchain.py
@@ -157,7 +157,7 @@ st.sidebar.header("About the App")
 
 # Add a description to the sidebar
 st.sidebar.write(
-    "Canadian Tax assistant app for personal tax filing for the year 2022-23. "
+    "Personal Tax assistant app for personal tax filing for the year 2022-23. "
     "This Langchain based App breaks the CRA tax guide into chunks of text and stores on a Pinecone vector DB(created with OpenAi embedding model)."
     "It accepts user queries and searches for relevant subsections from document to pass into OpenAI LLM QA model to get a response"
 )
